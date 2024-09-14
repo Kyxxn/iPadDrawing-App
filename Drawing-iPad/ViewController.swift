6699b0541e0c53ebe4a00401f827821fb3292385
Kyxxn <gywns626@naver.com>
2024-09-14 23:25:21 +0900

    chore: 기본 프로젝트 셋업

---
+++ Drawing-iPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drawing-iPad
+//
+//  Created by 박효준 on 9/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
